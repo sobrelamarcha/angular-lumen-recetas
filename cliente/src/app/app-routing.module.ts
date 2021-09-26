a4f82516c381c2647b91c7f69fee3902cca458bd
sobrelamarcha <sobrelamarcha@protonmail.com>
2021-09-26 19:16:39 +0200

    routing

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListarecetasComponent } from './componentes/listarecetas/listarecetas.component';
+import { NuevarecetaComponent } from './componentes/nuevareceta/nuevareceta.component';
+
+
+const routes: Routes = [
+  { path: '', component: ListarecetasComponent },
+  { path: 'nuevareceta', component: NuevarecetaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
